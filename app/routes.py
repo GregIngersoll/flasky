bbe34bf1869e41ee6379b82fe63cab839a3ba6e9
Greg Ingersoll <gpipuck@gmail.com>
2025-08-17 13:46:42 -0400

    Second push

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username':'Greg Ingersoll'}
+    posts = [
+        {
+            'author' : {'username':'Bob'},
+            'body' : 'Bob''s book'
+        },
+        {
+            'author' : {'username' : 'Sally' },
+            'body' : 'Sally''s book'
+        }
+
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
